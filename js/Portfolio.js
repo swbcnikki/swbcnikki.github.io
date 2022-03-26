b1d30078d0ab12724bd1826bc48139184589a564
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-03-25 21:47:35 -0700

    update slideshow and contact code

--- js/Portfolio.js
+++ js/Portfolio.js
@@ -34,11 +34,11 @@ function showSlides(n) {
         slides[i].style.display = "none"; // This for loop takes each item in the array "slides" and sets the display to none
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace("active", ""); // This for loop takes each item in the array "dots" and 
-        // removes "active" which removes the active styling
+        dots[i].className = dots[i].className.replace(" active", ""); // This for loop takes each item in the array "dots" and 
+        // removes "active" which removes the active styling. Please note the append is for " active". The space is important, it won't work right.
     }
     slides[slideIndex - 1].style.display = "block"; // This displays the image in the slideshow
-    dots[slideIndex - 1].className += "active"; // This adds the active styling to the dot associated with the image
+    dots[slideIndex - 1].className += " active"; // This adds the active styling to the dot associated with the image. Here also, it's append " active". The space is important.
 }
 // This code will create close the contact form when the user clicks off of it
 // The first step is to add an event listener for any clicks on the website
